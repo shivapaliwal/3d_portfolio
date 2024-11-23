e36ab351d7110c7120f0b8be7c737df547b203cf
Shivang Paliwal <shivapaliwal.hts@gmail.com>
2024-11-23 23:59:59 +0530

    navbar added

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
-import React from 'react'
 import {Route , BrowserRouter as Router ,Routes} from 'react-router-dom';
+import Navbar from './components/Navbar';
 
 const App = () => {
   return (
     <main className='bg-slate-300/20'>
         <Router>
-            Navbar
+            <Navbar/>
             <Routes>
-                <Route path='/' element={<Home />} />
-                <Route path='/about' element={<About />} />
-                <Route path='/contact' element={<Contact />} />
+                <Route path='/' element={"Home"} />
+                <Route path='/about' element={"About"} />
+                <Route path='/contact' element={"Contact"} />
             </Routes>
         </Router>
 
