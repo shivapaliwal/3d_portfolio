ee9bd0236c3eafa3cb9608f375c93dd4bb41e5ff
Shivang Paliwal <shivapaliwal.hts@gmail.com>
2024-11-26 00:19:47 +0530

    home island

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import {Route , BrowserRouter as Router ,Routes} from 'react-router-dom';
 import Navbar from './components/Navbar';
+import Home from './pages/Home';
 
 const App = () => {
   return (
@@ -7,7 +8,7 @@ const App = () => {
         <Router>
             <Navbar/>
             <Routes>
-                <Route path='/' element={"Home"} />
+                <Route path='/' element={<Home />} />
                 <Route path='/about' element={"About"} />
                 <Route path='/contact' element={"Contact"} />
             </Routes>
